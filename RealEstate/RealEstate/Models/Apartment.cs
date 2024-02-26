ed47989371dfa00f9726056f0e60c610004a2371
AzizbekStudent <00013836wiut@gmail.com>
2024-02-26 10:29:13 +0500

    Initial setup of Models
    Apartment
    Location
    Vendor

---
+++ RealEstate/RealEstate/Models/Apartment.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
+{
+    // Student ID: 00013836
+    public class Apartment
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "HouseTitle cannot be empty")]
+        public string? HouseTitle { get; set; }
+
+        [Required(ErrorMessage = "Description cannot be empty")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Area cannot be empty")]
+        public double? Area { get; set; }
+
+        [Required(ErrorMessage = "Price cannot be empty")]
+        public double? Price { get; set; }
+
+        [Required(ErrorMessage = "CompletionDate cannot be empty")]
+        public DateTime? CompletionDate { get; set; }
+
+        [Required(ErrorMessage = "IsForRent cannot be empty")]
+        public bool? IsForRent { get; set; } = false;
+
+        // Foreign key for address entity
+        public int? LocationId { get; set; }
+
+        [ForeignKey("LocationId")]
+        public Location? Location_ { get; set; }
+
+        // Foreign key for category entity
+        public int? VendorId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Vendor? Vendor_ { get; set; }
+    }
+}
