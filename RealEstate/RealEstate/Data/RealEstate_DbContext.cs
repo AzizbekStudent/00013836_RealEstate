a93005344f9079c35b94393689995b8296556b9f
AzizbekStudent <00013836wiut@gmail.com>
2024-02-26 10:39:05 +0500

    Dependecies added
    EntityFrameworkCore
    EntityFrameworkCore.SqlServer
    EntityFrameworkCore.Tools

---
+++ RealEstate/RealEstate/Data/RealEstate_DbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.Models;
+
+namespace RealEstate.Data
+{
+    // Student ID: 00013836
+    public class RealEstate_DbContext : DbContext
+    {
+        public RealEstate_DbContext(DbContextOptions<RealEstate_DbContext> options) : base(options) { }
+
+        // Models
+        public DbSet<Apartment> Apartments { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        public DbSet<Vendor> Vendors { get; set; }
+
+
+
+        // Setting relationships between entities
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // one to many 
+            // one location might have several apartments
+            modelBuilder.Entity<Apartment>()
+                .HasOne(address => address.Location_)
+                .WithMany()
+                .HasForeignKey(address => address.LocationId);
+
+            // one to many
+            // one vendor might sell several apartments
+            modelBuilder.Entity<Apartment>()
+                .HasOne(category => category.Vendor_)
+                .WithMany()
+                .HasForeignKey(category => category.VendorId);
+        }
+    }
+}
--- RealEstate/RealEstate/Program.cs
+++ RealEstate/RealEstate/Program.cs
@@ -1,5 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
 
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var _connStr = "SqlServer_Connection";
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -8,6 +12,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Initializing RealEstate_DbContext
+try
+{
+    builder.Services.AddDbContext<RealEstate_DbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString(_connStr)));
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error configuring DbContext: {ex.Message}");
+    throw; // Ensure the exception is propagated
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
